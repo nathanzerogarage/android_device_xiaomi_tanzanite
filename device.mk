31a7776d2c16a4eefdc843a38de513645b9541a7
Nathan <nathannxx@proton.me>
2026-08-19 03:09:07 +0700

    fixup! tanzanite: Switch back to prebuilt IMS stack

--- device.mk
+++ device.mk
@@ -172,7 +172,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml \
     frameworks/native/data/etc/android.software.ipsec_tunnel_migration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnel_migration.xml \
-    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
+    frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
@@ -377,6 +379,7 @@ PRODUCT_PACKAGES += \
     SettingsResOverlayTanzanite \
     SettingsProviderResOverlayTanzanite \
     SystemUIOverlayTanzanite \
+    TelephonyOverlayTanzanite \
     TetheringResOverlayTanzanite \
     WifiResOverlayTanzanite
 
